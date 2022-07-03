4d3ede50d243d93e068359d21394243bd32124ff
Adam Birnbaum <adambiren19@gmail.com>
2022-07-03 20:01:33 +0300

    added persistence via localStorage, a delete function and design

--- src/Components/Page/CRUD.js
+++ src/Components/Page/CRUD.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import './CRUD.css'
 
 function Crud() {
-
+    localStorage.setItem("todos")
     const localArr = localStorage.getItem("todos");
 
     const [title, setTitle] = useState("");
